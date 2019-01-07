9dd5cf6a32394f26e14334016863b30fa74fd5ea
Pete <petethegoat@gmail.com>
2019-01-07 14:37:15 -0600

    Added an icon during barter to indicate items stolen from that merchant. Utilize tes3.bookType. Fix possible nil reference. Beautified extraTooltipLate (somewhat)

--- User Interface Expansion/MWSE/mods/UI Expansion/tooltip.lua
+++ User Interface Expansion/MWSE/mods/UI Expansion/tooltip.lua
@@ -153,8 +153,8 @@ local function replaceBookTooltip(tooltip, book, itemData)
 	for i = #tooltip:getContentElement().children, 2, -1 do
 		tooltip:getContentElement().children[i].visible = false
 	end
-	--TODO: magic number
-	if book.type == 1 then
+
+	if book.type == tes3.bookType.scroll then
 		enchantConditionBlock(tooltip, book, itemData)
 	end
 end
@@ -184,102 +184,98 @@ end
 
 local function extraTooltipEarly(e)
 	-- I believe this is hardcoded in engine, so we'll just do this too.
-	if e.object.id:find("Gold_") or e.object.isKey then
-		return
-	end
-
-	-- Adjust and remove vanilla tooltip fields.
-	local parent = e.tooltip.children[1]
-	-- Iterate in reverse so we can just destroy the elements as we find them.
-	for i = #parent.children, 1, -1 do
-		for k, field in pairs(hiddenDefaultFields) do
-			if parent.children[i].text:find(field) then
-				parent.children[i].visible = false
-				break
+	if not e.object.id:find("Gold_") and not e.object.isKey then
+		-- Adjust and remove vanilla tooltip fields.
+		local parent = e.tooltip.children[1]
+		-- Iterate in reverse so we can just destroy the elements as we find them.
+		for i = #parent.children, 1, -1 do
+			for k, field in pairs(hiddenDefaultFields) do
+				if parent.children[i].text:find(field) then
+					parent.children[i].visible = false
+					break
+				end
 			end
 		end
-	end
 
-	-- Add padding to the title.
-	e.tooltip:getContentElement().children[1].borderAllSides = 3
+		-- Add padding to the title.
+		e.tooltip:getContentElement().children[1].borderAllSides = 3
 
-	if e.object.objectType == tes3.objectType.weapon or e.object.objectType == tes3.objectType.ammunition then
-		replaceWeaponTooltip(e.tooltip, e.object, e.itemData)
-	elseif e.object.objectType == tes3.objectType.armor then
-		replaceArmorTooltip(e.tooltip, e.object, e.itemData)
-	elseif e.object.objectType == tes3.objectType.clothing then
-		replaceClothingTooltip(e.tooltip, e.object, e.itemData)
-	elseif e.object.objectType == tes3.objectType.book then
-		replaceBookTooltip(e.tooltip, e.object, e.itemData)
-	elseif e.object.objectType == tes3.objectType.alchemy then
-		replaceAlchemyTooltip(e.tooltip, e.object, e.itemData)
+		if e.object.objectType == tes3.objectType.weapon or e.object.objectType == tes3.objectType.ammunition then
+			replaceWeaponTooltip(e.tooltip, e.object, e.itemData)
+		elseif e.object.objectType == tes3.objectType.armor then
+			replaceArmorTooltip(e.tooltip, e.object, e.itemData)
+		elseif e.object.objectType == tes3.objectType.clothing then
+			replaceClothingTooltip(e.tooltip, e.object, e.itemData)
+		elseif e.object.objectType == tes3.objectType.book then
+			replaceBookTooltip(e.tooltip, e.object, e.itemData)
+		elseif e.object.objectType == tes3.objectType.alchemy then
+			replaceAlchemyTooltip(e.tooltip, e.object, e.itemData)
 
-	-- Light duration
-	elseif e.object.objectType == tes3.objectType.light then
-		local blockDurationBar = e.tooltip:createBlock({})
-		blockDurationBar.autoWidth = true
-		blockDurationBar.autoHeight = true
-		blockDurationBar.paddingAllSides = 4
-		blockDurationBar.paddingLeft = 2
-		blockDurationBar.paddingRight = 2
-		blockDurationBar:createLabel{text = string.format("%s:", common.dictionary.lightDuration)}
+		-- Light duration
+		elseif e.object.objectType == tes3.objectType.light then
+			local blockDurationBar = e.tooltip:createBlock({})
+			blockDurationBar.autoWidth = true
+			blockDurationBar.autoHeight = true
+			blockDurationBar.paddingAllSides = 4
+			blockDurationBar.paddingLeft = 2
+			blockDurationBar.paddingRight = 2
+			blockDurationBar:createLabel{text = string.format("%s:", common.dictionary.lightDuration)}
 
-		local labelDurationBar = blockDurationBar:createFillBar{current = e.itemData and e.itemData.timeLeft or e.object.time, max = e.object.time}
-		--TODO see if there's a better color
-		--labelDurationBar.widget.fillColor = tes3ui.getPalette("normal_color")
-		labelDurationBar.borderLeft = 4
+			local labelDurationBar = blockDurationBar:createFillBar{current = e.itemData and e.itemData.timeLeft or e.object.time, max = e.object.time}
+			labelDurationBar.borderLeft = 4
 
-	-- Soul gem capacity
-	elseif e.object.isSoulGem then
-		local soulValue = tes3.findGMST(tes3.gmst.fSoulGemMult).value * e.object.value
-		labelBlock(e.tooltip, string.format("%s: %u", common.dictionary.soulCapacity, soulValue))
-	end
+		-- Soul gem capacity
+		elseif e.object.isSoulGem then
+			local soulValue = tes3.findGMST(tes3.gmst.fSoulGemMult).value * e.object.value
+			labelBlock(e.tooltip, string.format("%s: %u", common.dictionary.soulCapacity, soulValue))
+		end
 
-	-- Add the value and weight back in.
-	if e.object.value and e.object.weight then
-		local container = e.tooltip:createBlock({id = GUI_ID_TooltipIconBar})
-		container.widthProportional = 1.0
-		container.minHeight = 16
-		container.autoHeight = true
-		container.paddingAllSides = 2
-		container.paddingTop = 4
-		container.childAlignX = 1.0
+		-- Add the value and weight back in.
+		if e.object.value and e.object.weight then
+			local container = e.tooltip:createBlock({id = GUI_ID_TooltipIconBar})
+			container.widthProportional = 1.0
+			container.minHeight = 16
+			container.autoHeight = true
+			container.paddingAllSides = 2
+			container.paddingTop = 4
+			container.childAlignX = 1.0
 
-		-- Value
-		local block = container:createBlock({})
-		block.autoWidth = true
-		block.autoHeight = true
-		block:createImage{ path = "icons/gold.dds" }
-		local label = block:createLabel{ text = string.format("%u", e.object.value) }
-		label.borderLeft = 4
+			-- Value
+			local block = container:createBlock({})
+			block.autoWidth = true
+			block.autoHeight = true
+			block:createImage{ path = "icons/gold.dds" }
+			local label = block:createLabel{ text = string.format("%u", e.object.value) }
+			label.borderLeft = 4
 
-		-- Weight
-		block = container:createBlock({})
-		block.autoWidth = true
-		block.autoHeight = true
-		block:createImage{ path = "icons/weight.dds" }
-		block.borderLeft = 8
-		label = block:createLabel{ text = string.format("%.2f", e.object.weight) }
-		label.borderLeft = 4
+			-- Weight
+			block = container:createBlock({})
+			block.autoWidth = true
+			block.autoHeight = true
+			block:createImage{ path = "icons/weight.dds" }
+			block.borderLeft = 8
+			label = block:createLabel{ text = string.format("%.2f", e.object.weight) }
+			label.borderLeft = 4
 
-		parent:updateLayout()
+			parent:updateLayout()
 
-		-- Update minimum width of the whole tooltip to make sure there's space for the value/weight.
-		e.tooltip:getContentElement().minWidth = 120
-		e.tooltip:updateLayout()
-	end
+			-- Update minimum width of the whole tooltip to make sure there's space for the value/weight.
+			e.tooltip:getContentElement().minWidth = 120
+			e.tooltip:updateLayout()
+		end
 
-	-- Show a tooltip for stolen goods!
-	local merchant = tes3ui.getServiceActor()
-	if merchant ~= nil and e.object.stolenList ~= nil then
-		for i, v in pairs(e.object.stolenList) do
-			if merchant.object.name == v.name then
-				e.tooltip:createDivider()
-				local label = labelBlock(e.tooltip, common.dictionary.stolenFromMerchant)
-				label.borderAllSides = 8
-				label.justifyText = "center"
-				label.color = tes3ui.getPalette("negative_color")
-				break
+		-- Show a tooltip for stolen goods!
+		local merchant = tes3ui.getServiceActor()
+		if merchant ~= nil and e.object.stolenList ~= nil then
+			for i, v in pairs(e.object.stolenList) do
+				if merchant.object.name == v.name then
+					e.tooltip:createDivider()
+					local label = labelBlock(e.tooltip, common.dictionary.stolenFromMerchant)
+					label.borderAllSides = 6
+					label.justifyText = "center"
+					label.color = tes3ui.getPalette("negative_color")
+					break
+				end
 			end
 		end
 	end
@@ -289,22 +285,47 @@ local function extraTooltipEarly(e)
 	divide.widthProportional = 0.85
 	divide.visible = false
 end
-event.register("uiObjectTooltip", extraTooltipEarly, {priority = 100})
 
 local function extraTooltipLate(e)
+	local element = e.tooltip:getContentElement()
+	local children = element.children
+
 	-- If our divider isn't the last element, then something else was added, like flavor text.
-	if e.tooltip:getContentElement().children[#e.tooltip:getContentElement().children].id ~= GUI_ID_TooltipExtraDivider then
-		local divide = e.tooltip:getContentElement():findChild(GUI_ID_TooltipExtraDivider)
+	if children[#children].id ~= GUI_ID_TooltipExtraDivider then
+		local divide = element:findChild(GUI_ID_TooltipExtraDivider)
 		divide.visible = true
 	end
 
 	-- Now, we'll make sure our icon bar is in the position we want (currently the very bottom).
 	--TODO: add MCM option to set the position of the iconbar. Top, above enchants, above flavortext, bottom.
-	for i = #e.tooltip:getContentElement().children, 1, -1 do
-		if e.tooltip:getContentElement().children[i].id == GUI_ID_TooltipIconBar then
-			e.tooltip:getContentElement():reorderChildren(#e.tooltip:getContentElement().children, i - 1, 1)
+	for i = #children, 1, -1 do
+		if children[i].id == GUI_ID_TooltipIconBar then
+			element:reorderChildren(#children, i - 1, 1)
 			break
 		end
 	end
 end
+
+-- Register our events, one early, and one late.
+event.register("uiObjectTooltip", extraTooltipEarly, {priority = 100})
 event.register("uiObjectTooltip", extraTooltipLate, {priority = -100})
+
+local function onItemTileUpdated(e)
+	-- Show an indicator for stolen goods!
+	local merchant = tes3ui.getServiceActor()
+	if merchant ~= nil and e.item.stolenList ~= nil then
+		for i, v in pairs(e.item.stolenList) do
+			if merchant.object.name == v.name then
+				local icon = e.element:createImage({ path = "icons/ui_exp/ownership_indicator.dds" })
+				icon.consumeMouseEvents = false
+				icon.width = 16
+				icon.height = 16
+				icon.scaleMode = true
+				icon.absolutePosAlignX = 0.75
+				icon.absolutePosAlignY = 0.75
+				break
+			end
+		end
+	end
+end
+event.register("itemTileUpdated", onItemTileUpdated, {filter = "MenuInventory"})
