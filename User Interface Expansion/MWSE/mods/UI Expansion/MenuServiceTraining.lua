b0619173bc75cb348e39a832ec4764a6b08a6b47
unknown <iwill@nottellyou.com>
2019-04-10 17:22:01 +0200

    Tabs instead of spaces

--- User Interface Expansion/MWSE/mods/UI Expansion/MenuServiceTraining.lua
+++ User Interface Expansion/MWSE/mods/UI Expansion/MenuServiceTraining.lua
@@ -2,7 +2,7 @@ local common = require("UI Expansion.common")
 local lasttarget
 
 local function onAfterTrainTimer()
-    if(tes3.menuMode()) then
+	if(tes3.menuMode()) then
 		timer.start({ duration = 0.3, callback = onAfterTrainTimer })
 	else
 		if(lasttarget ~= nil) then		
@@ -15,12 +15,12 @@ local function onAfterTrainTimer()
 end
 
 local function openTraining(e)
-    e.source:forwardEvent(e)
+	e.source:forwardEvent(e)
 	timer.start({ duration = 0.3, callback = onAfterTrainTimer })
 end
 
 local function menuTraining(e)
-    --local scp = e.element:findChild(tes3ui.registerID("PartScrollPane_Pane"))
+	--local scp = e.element:findChild(tes3ui.registerID("PartScrollPane_Pane"))
 	local scp = e.element.children[2].children[3].children[1].children[1]
 	for i,v in ipairs(scp.children) do 
 		v.children[1]:register("mouseClick", openTraining)
