056c4e5dc661b8ad19bca14bc21ccab6e38c9bc7
NullCascade <nullcascade@gmail.com>
2019-05-02 20:01:42 -0700

    The Snap: Add fader to white out the screen for the snap event. Still needs a texture made to fully white it out, since I couldn't find a pure white texture and am too lazy to open photoshop.

--- The Snap/MWSE/mods/The Snap/main.lua
+++ The Snap/MWSE/mods/The Snap/main.lua
@@ -2,6 +2,8 @@
 local infinityGauntlet = nil
 local dust = nil
 
+local snapFader = nil
+
 local function snapReference(reference)
 	-- Only snap living things.
 	local objectType = reference.object.objectType
@@ -17,14 +19,12 @@ local function snapReference(reference)
 
 	if (math.random(1, 2) == 1) then
 		data.nc_snapped = true
-		mwse.log("%s was snapped!", reference)
 
 		mwscript.drop({ reference = reference, item = dust, distance = 0 })
 		mwscript.disable({ reference = reference })
 		mwscript.setDelete({ reference = reference })
 	else
 		data.nc_snapped = false
-		mwse.log("%s was spared.", reference)
 	end
 end
 
@@ -52,17 +52,36 @@ end
 event.register("cellChanged", snapCells)
 event.register("loaded", snapCells)
 
+local function startFaderWithCallback(params)
+	local fader = params.fader
+	fader:fadeTo(params)
+	params.duration = params.duration + (params.duration + params.delay)
+	timer.start(params)
+end
+
 local function onInfinityGauntletEquipped(e)
 	if (tes3.player.data.nc_worldSnapped) then
 		return
 	end
 
 	tes3.player.data.nc_worldSnapped = true
-	snapCells()
-	tes3.playSound({
-		reference = tes3.player,
-		soundPath = "Fx\\thanos\\snap.wav",
-	})
+
+	snapFader:activate()
+
+	startFaderWithCallback({ fader = snapFader, value = 0.0, duration = 0.01, delay = 2.0, callback = function()
+		startFaderWithCallback({ fader = snapFader, value = 1.0, duration = 0.5, delay = 1.5, callback = function()
+			tes3.playSound({
+				reference = tes3.player,
+				soundPath = "Fx\\thanos\\snap.wav",
+			})
+
+			snapCells()
+	
+			startFaderWithCallback({ fader = snapFader, value = 0.0, duration = 3.0, delay = 0.5, callback = function()
+				snapFader:deactivate()
+			end})
+		end})
+	end})
 end
 
 local function onInitialized()
@@ -72,3 +91,17 @@ local function onInitialized()
 	event.register("equipped", onInfinityGauntletEquipped, { filter = infinityGauntlet })
 end
 event.register("initialized", onInitialized)
+
+local function updateSnapFader()
+	snapFader:update()
+end
+
+local function faderSetup()
+	-- Create the tentacle fader.
+	snapFader = tes3fader.new()
+	snapFader:setTexture("Textures\\Tx_Sky_Foggy.dds")
+	snapFader:setColor({ color = { 1.0, 1.0, 1.0 }, flag = false })
+	
+	event.register("enterFrame", updateSnapFader)
+end
+event.register("initialized", faderSetup)
