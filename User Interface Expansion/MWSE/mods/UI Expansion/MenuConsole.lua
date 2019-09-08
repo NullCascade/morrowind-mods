NullCascade/morrowind-mods

commit 0ec381d85df971d3371f0763734e9151c4dc4798

Add (modified) modifications by abot. His changes:
- added numeric hotkeys/prefix to Dialog Choices in MenuDialog.lua
- added automatic MenuMap.lua compatibility with abot/Smart Map
- better compatibility with Luacheck hints in Visual Studio Code
- added clear/clickable icon for search fields

User Interface Expansion/MWSE/mods/UI Expansion/MenuConsole.lua
@@ -5,8 +5,6 @@ local GUI_ID_MenuConsole_scroll_pane = tes3ui.registerID("MenuConsole_scroll_pan
 
 local GUI_ID_UIEXP_ConsoleInputBox = tes3ui.registerID("UIEXP:ConsoleInputBox")
 
-local common = require("UI Expansion.common")
-
 local luaMode = false
 local currentHistoryIndex = 1
 local previousEntries = { { text = "", lua = false } }
@@ -26,14 +24,14 @@ function sandboxInit()
     setmetatable(sandbox, { __index = _G })
     sandbox.print = tes3ui.logToConsole
 end
-    
+
 function sandboxScript(f)
     sandbox.currentRef = tes3ui.findMenu(GUI_ID_MenuConsole):getPropertyObject("MenuConsole_current_ref")
     setfenv(f, sandbox)
     return pcall(f)
 end
 
-local function onSubmitCommand(e)
+local function onSubmitCommand()
 	local menuConsole = tes3ui.findMenu(GUI_ID_MenuConsole)
 	local inputBox = menuConsole:findChild(GUI_ID_UIEXP_ConsoleInputBox)
 	local text = inputBox.text
@@ -41,13 +39,13 @@ local function onSubmitCommand(e)
 
 	if (luaMode) then
 		tes3ui.logToConsole(text, true)
-		
+
 		-- Try compiling command as an expression first.
 		local f, message = loadstring("return " .. text)
 		if (not f) then
 			f, message = loadstring(text)
 		end
-		
+
 		-- Run command and show output in console.
 		if (f) then
 			local status, errorOrResult = sandboxScript(f)
@@ -65,7 +63,7 @@ local function onSubmitCommand(e)
 		-- Any of the togglestats reporting outputs will destroy the text input,
 		-- which is recreated on the next key input, so send one.
 		menuConsole:triggerEvent("keyEnter")
-		
+
 		local vanillaInputText = menuConsole:findChild(GUI_ID_MenuConsole_text_input)
 		vanillaInputText.text = text
 		menuConsole:triggerEvent("keyEnter")
@@ -92,11 +90,11 @@ local function onMenuConsoleActivated(e)
 		tes3ui.acquireTextInput(e.element:findChild(GUI_ID_UIEXP_ConsoleInputBox))
 		return
 	end
-	
+
 	local menuConsole = e.element
 	local mainPane = menuConsole:findChild(GUI_ID_MenuConsole_scroll_pane).parent
 	mainPane.borderBottom = 0
-	
+
 	-- Disable normal input method.
 	local vanillaInputText = menuConsole:findChild(GUI_ID_MenuConsole_text_input)
 	vanillaInputText.visible = false
@@ -132,7 +130,7 @@ local function onMenuConsoleActivated(e)
 	scriptToggleButton.borderLeft = 4
 	scriptToggleButton.minWidth = 90
 	scriptToggleButton.width = 90
-	scriptToggleButton:register("mouseClick", function(e)
+	scriptToggleButton:register("mouseClick", function()
 		luaMode = not luaMode
 		updateScriptButton(scriptToggleButton)
 		menuConsole:updateLayout()
@@ -141,7 +139,7 @@ local function onMenuConsoleActivated(e)
 	toggleText.wrapText = true
 	toggleText.justifyText = "center"
 
-	input:register("keyPress", function(e)
+	input:register("keyPress", function()
 		local key = e.data0
 
 		if (key == 9) then

User Interface Expansion/MWSE/mods/UI Expansion/MenuBarter.lua
@@ -41,7 +41,7 @@ local function onMenuBarterActivated(e)
 	end
 
 	-- Hide it again when this UI goes away.
-	buttonBlock:register("destroy", function(e)
+	buttonBlock:register("destroy", function()
 		common.allFilters.inventory:setFilterHidden("tradable", true)
 	end)
 

User Interface Expansion/MWSE/mods/UI Expansion/MenuContents.lua
@@ -19,7 +19,9 @@ local function onKeyInput()
 	if (common.complexKeybindTest(common.config.keybindTakeAll)) then
 		local contentsMenu = tes3ui.findMenu(GUI_ID_MenuContents)
 		local takeAllButton = contentsMenu:findChild(GUI_ID_MenuContents_takeallbutton)
-		takeAllButton:triggerEvent("mouseClick")
+		if takeAllButton then --- it may be nil /abot
+			takeAllButton:triggerEvent("mouseClick")
+		end
 		return false
 	-- Space (when no text) closes.
 	elseif (common.allFilters.contents:getSearchText() == nil and common.complexKeybindTest(common.config.keybindClose)) then
@@ -35,11 +37,13 @@ local function onFilterChanged()
 			return
 		end
 		local takeAllButton = contentsMenu:findChild(GUI_ID_MenuContents_takeallbutton)
-		local contentsFilter = common.allFilters.contents
-		if (contentsFilter.searchText ~= nil or #contentsFilter.filtersOrdered ~= #contentsFilter.activeFilters) then
-			takeAllButton.text = common.dictionary.takeFiltered
-		else
-			takeAllButton.text = common.dictionary.takeAll
+		if takeAllButton then --- it may be nil /abot
+			local contentsFilter = common.allFilters.contents
+			if (contentsFilter.searchText ~= nil or #contentsFilter.filtersOrdered ~= #contentsFilter.activeFilters) then
+				takeAllButton.text = common.dictionary.takeFiltered
+			else
+				takeAllButton.text = common.dictionary.takeAll
+			end
 		end
 	end
 	tes3ui.updateContentsMenuTiles()
@@ -84,9 +88,8 @@ local function onMenuContentsActivated(e)
 
 	-- Register a key event for take all and container closing.
 	event.register("keyDown", onKeyInput)
-	event.register("menuExit", function (e)
-		event.unregister("keyDown", onKeyInput,
-		{ doOnce = true })
+	event.register("menuExit", function()
+		event.unregister("keyDown", onKeyInput, { doOnce = true })
 	end)
 
 	-- Add a new block in the right place.

User Interface Expansion/MWSE/mods/UI Expansion/MenuDialog.lua
@@ -17,6 +17,22 @@ local GUI_Palette_TopicUnique = common.getColor(common.config.dialogueTopicUniqu
 -- Dialogue: Adds colorization to show what topics provide new and unique responses.
 ----------------------------------------------------------------------------------------------------
 
+--[[
+adds number prefix to Choice answers and makes them working pressing the number key /abot
+--]]
+local answers = {}
+
+local function keyDown(e)
+	local key = e.keyCode - 1
+	if answers then
+		local answer = answers[key]
+		if answer then
+			tes3.messageBox(answer.text)
+			answer:triggerEvent("mouseClick")
+		end
+	end
+end
+
 local function updateTopicsList(e)
 	-- If the function lacks context to the dialogue menu, look it up.
 	local menuDialogue = tes3ui.findMenu(GUI_ID_MenuDialog)
@@ -29,19 +45,31 @@ local function updateTopicsList(e)
 
 		-- Were we forced out of dialogue?
 		if (tes3ui.findMenu(GUI_ID_MenuDialog) == nil) then
+			answers = {}
 			return
 		end
 
 		-- Make sure that the first heard from field is always used.
-		if (e.info and e.actor and e.info.firstHeardFrom == nil and textPane:findChild(GUI_ID_MenuDialog_answer_block) == nil) then
+		if e.info and e.actor and (e.info.firstHeardFrom == nil)
+		and (textPane:findChild(GUI_ID_MenuDialog_answer_block) == nil) then
 			e.info.firstHeardFrom = e.actor
 		end
 	end
 
 	-- Catch events from hyperlinks.
+
+	local i = 0
+	answers = {}
 	for _, element in pairs(textPane.children) do
 		if (element.id == GUI_ID_MenuDialog_hyper or element.id == GUI_ID_MenuDialog_answer_block) then
 			element:register("mouseClick", updateTopicsList)
+		end
+		if element.id == GUI_ID_MenuDialog_answer_block then
+			i = i + 1
+			if string.match(element.text,"^%d+") == nil then
+				element.text = string.format("%s %s", i, element.text)
+				answers[i] = element
+			end
 		end
 	end
 
@@ -75,7 +103,6 @@ local function updateTopicsList(e)
 			element:register("mouseClick", function(mouseClickEventData)
 				mouseClickEventData.info = info
 				mouseClickEventData.actor = actor
-
 				updateTopicsList(mouseClickEventData)
 			end)
 		end
@@ -89,11 +116,12 @@ local function onDialogueMenuActivated(e)
 	end
 
 	-- Set the pre-update event to update the topic list.
+	answers = {}
 	e.element:register("preUpdate", function(preUpdateEventData)
 		-- We only want this event to fire once. We'll manually track changes above to be more efficient.
 		e.element:unregister("preUpdate")
-
 		updateTopicsList(preUpdateEventData)
 	end)
 end
 event.register("uiActivated", onDialogueMenuActivated, { filter = "MenuDialog" })
+event.register("keyDown", keyDown)

User Interface Expansion/MWSE/mods/UI Expansion/MenuInventory.lua
@@ -1,11 +1,9 @@
 
 local GUI_ID_MenuBarter = tes3ui.registerID("MenuBarter")
 local GUI_ID_MenuInventory_button_layout = tes3ui.registerID("MenuInventory_button_layout")
-local GUI_ID_MenuInventory_scrollpane = tes3ui.registerID("MenuInventory_scrollpane")
 
 local common = require("UI Expansion.common")
 
-local inputController = tes3.worldController.inputController
 
 ----------------------------------------------------------------------------------------------------
 -- Inventory: Searching and filtering.
@@ -91,12 +89,12 @@ local function onMenuInventoryActivated(e)
 end
 event.register("uiActivated", onMenuInventoryActivated, { filter = "MenuInventory" } )
 
-local function onMenuBarterActivated(e)
+local function onMenuBarterActivated()
 	currentMerchant = tes3ui.getServiceActor()
 end
 event.register("uiActivated", onMenuBarterActivated, { filter = "MenuBarter" } )
 
-local function onEnterMenuMode(e)
+local function onEnterMenuMode()
 	inventoryFilters:setFilterHidden("tradable", true)
 	inventoryFilters:clearFilter()
 

User Interface Expansion/MWSE/mods/UI Expansion/MenuMagic.lua
@@ -2,7 +2,6 @@
 local GUI_ID_MagicMenu_spells_list = tes3ui.registerID("MagicMenu_spells_list")
 local GUI_ID_MenuMagic = tes3ui.registerID("MenuMagic")
 
-local GUI_ID_UIEXP_MagicMenu_SchoolFilters = tes3ui.registerID("UIEXP_MagicMenu_SchoolFilters")
 
 local common = require("UI Expansion.common")
 
@@ -15,7 +14,7 @@ local firstSearchResult = nil
 local function searchSubList(titleElement, listElement, isSpellFilter)
 	-- Gather a list of all the columns/rows so we don't have to keep creating tables later.
 	local columnElements = {}
-	for i, element in ipairs(listElement.children) do
+	for _, element in ipairs(listElement.children) do
 		table.insert(columnElements, element.children)
 	end
 
@@ -85,9 +84,13 @@ local magicFilters = common.createFilterInterface({
 
 local function getEffectsContainsSchool(effects, school)
 	for i = 1, #effects do
-		local obj = effects[i].object
-		if (obj and obj.school == school) then
-			return true
+		local eff = effects[i]
+		if eff then
+			if eff.object then
+				if eff.object.school == school then
+					return true
+				end
+			end
 		end
 	end
 	return false
@@ -178,9 +181,9 @@ local function onMenuMagicActivated(e)
 end
 event.register("uiActivated", onMenuMagicActivated, { filter = "MenuMagic" } )
 
-local function onEnterMenuMode(e)
+local function onEnterMenuMode()
 	magicFilters:clearFilter()
-	
+
 	if (common.config.autoSelectInput == "Magic") then
 		magicFilters:focusSearchBar()
 	end

User Interface Expansion/MWSE/mods/UI Expansion/MenuMap.lua
@@ -1,9 +1,15 @@
 local common = require("UI Expansion.common")
 
+local GUIID_MenuMap = tes3ui.registerID("MenuMap")
 local GUIID_MenuMap_switch = tes3ui.registerID("MenuMap_switch")
-local MenuMap_switch = nil
 
 local function changeCell(e)
+	local MenuMap = tes3ui.findMenu(GUIID_MenuMap)
+	if (MenuMap == nil) then
+		return
+	end
+
+	local MenuMap_switch = menuMap:findChild(GUIID_MenuMap_switch)
 	if (MenuMap_switch == nil) then
 		return
 	end
@@ -15,17 +21,26 @@ local function changeCell(e)
 		MenuMap_switch:triggerEvent("mouseClick")
 	end
 end
-event.register("cellChanged", changeCell)
 
+-- SmartMap compatibility
+local lfs = require("lfs")
+if lfs.attributes("Data Files/MWSE/mods/abot/Smart Map/main.lua") then
+	mwse.log("[UI Expansion] MenuMap: skipping cellChanged event to be managed by abot/Smart Map");
+else
+	event.register("cellChanged", changeCell)
+end
 
 local function onKeyInput()
 	if (common.complexKeybindTest(common.config.keybindMapSwitch)) then
-		MenuMap_switch:triggerEvent("mouseClick")
+		local MenuMap = tes3ui.findMenu(GUIID_MenuMap)
+		if (MenuMap == nil) then
+			return
+		end
+		
+		local MenuMap_switch = menuMap:findChild(GUIID_MenuMap_switch)
+		if MenuMap_switch then
+			MenuMap_switch:triggerEvent("mouseClick")
+		end
 	end
 end
 event.register("keyDown", onKeyInput)
-
-local function onMapCreated(e)
-	MenuMap_switch = e.element:findChild(GUIID_MenuMap_switch)
-end
-event.register("uiActivated", onMapCreated, { filter = "MenuMap" } )

User Interface Expansion/MWSE/mods/UI Expansion/MenuMapPlugin.lua
@@ -1,4 +1,4 @@
-local common = require("UI Expansion.common")
+---local common = require("UI Expansion.common")
 
 -- Hook map changes.
 local extern = include("uiextension")
@@ -25,7 +25,7 @@ local function onMapMenuActivated(e)
 	zoomBar = newBottomBlock:createSlider({ current = 0, max = 300, step = 1, jump = 10 })
 	zoomBar.absolutePosAlignY = 0.5
 	zoomBar.width = 300
-	zoomBar:register("PartScrollBar_changed", function(e)
+	zoomBar:register("PartScrollBar_changed", function()
 		currentZoom = zoomBar:getPropertyInt("PartScrollBar_current")/100 + 1.0
 		extern.setMapZoom(currentZoom)
 	end)

commit e1f0a98f04b598445e1976eae310bfc18746d947

Fix console that broke in 0ec381d85df971d3371f0763734e9151c4dc4798.

User Interface Expansion/MWSE/mods/UI Expansion/MenuConsole.lua
@@ -139,7 +139,7 @@ local function onMenuConsoleActivated(e)
 	toggleText.wrapText = true
 	toggleText.justifyText = "center"
 
-	input:register("keyPress", function()
+	input:register("keyPress", function(e)
 		local key = e.data0
 
 		if (key == 9) then