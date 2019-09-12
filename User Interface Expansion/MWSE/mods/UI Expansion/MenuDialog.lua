3eb4d9c65c30d55407d51ce681fcf62ebbdf7581
Pete <petethegoat@gmail.com>
2019-09-12 13:02:49 -0500

    abot's fixes for keyboard input on answers- works on greetings now!

--- User Interface Expansion/MWSE/mods/UI Expansion/MenuDialog.lua
+++ User Interface Expansion/MWSE/mods/UI Expansion/MenuDialog.lua
@@ -23,18 +23,42 @@ local answers = {}
 local function checkForAnswerHotkey(e)
 	-- Make sure we're in the dialogue menu.
 	local topMenu = tes3.getTopMenu()
-	if (topMenu == nil or topMenu.id ~= GUI_ID_MenuDialog) then
+	if not topMenu then
+		---mwse.log("checkForAnswerHotkey: topMenu = %s", topMenu)
+		return
+	end
+	if not (topMenu.id == GUI_ID_MenuDialog) then
+		---mwse.log("checkForAnswerHotkey: topMenu.id = %s, GUI_ID_MenuDialog = %s", topMenu.id, GUI_ID_MenuDialog)
 		return
 	end
 
 	-- Make sure we have answers.
-	if answers == nil or #answers == 0 then
+	if not answers then
+		---mwse.log("checkForAnswerHotkey: answers = %s", answers)
+		return
+	end
+	if #answers <= 0 then
+		---mwse.log("checkForAnswerHotkey: #answers = %s", #answers)
 		return
 	end
 
 	local key = tes3.scanCodeToNumber[e.keyCode]
+	if not key then
+		return
+	end
+
 	local answer = answers[key]
+	if not answer then
+		---mwse.log("checkForAnswerHotkey: key = %s, answer not found", key)
+		return
+	end
+
 	if answer then
+		local s = string.match(answer.text, "^%d+%. (.+)$")
+		if not s then
+			s = answer.txt
+		end
+		tes3.messageBox(s)
 		answer:triggerEvent("mouseClick")
 	end
 end
@@ -51,34 +75,32 @@ local function updateTopicsList(e)
 
 		-- Were we forced out of dialogue?
 		if (tes3ui.findMenu(GUI_ID_MenuDialog) == nil) then
+			---mwse.log("updateTopicsList tes3ui.findMenu(GUI_ID_MenuDialog) == nil");
 			answers = {}
 			return
 		end
 
-		-- Make sure that the first heard from field is always used.
-		if e.info and e.actor and (e.info.firstHeardFrom == nil)
-		and (textPane:findChild(GUI_ID_MenuDialog_answer_block) == nil) then
-			e.info.firstHeardFrom = e.actor
+		-- Make sure that the node heard from field is always used.
+		if e.info then
+			---mwse.log("[UI Expansion] e.info = %s", e.info);
+			if e.actor then
+				---mwse.log("[UI Expansion] e.actor = %s", e.actor);
+				if (e.info.firstHeardFrom == nil) then
+					if (textPane:findChild(GUI_ID_MenuDialog_answer_block) == nil) then
+						e.info.firstHeardFrom = e.actor
+					end
+				end
+				---mwse.log("[UI Expansion] e.info.firstHeardFrom = %s", e.info.firstHeardFrom)
+			end
 		end
 	end
 
 	-- Catch events from hyperlinks.
-	local answerIndex = 1
-	answers = {}
 	for _, element in pairs(textPane.children) do
+		---mwse.log("[UI Expansion] _ = %s, element = %s, element.id = %s, %s", _, element, element.id, element.text)
 		if (element.id == GUI_ID_MenuDialog_hyper) then
+			---mwse.log("[UI Expansion] GUI_ID_MenuDialog_hyper = element.id = %s, %s", element.id, element.text)
 			element:register("mouseClick", updateTopicsList)
-		elseif (element.id == GUI_ID_MenuDialog_answer_block) then
-			local oldText = element.text
-			element:register("mouseClick", function(e)
-				tes3.messageBox(oldText)
-				updateTopicsList(e)
-			end)
-			if string.match(element.text,"^%d+") == nil then
-				element.text = string.format("%d. %s", answerIndex, element.text)
-				answers[answerIndex] = element
-				answerIndex = answerIndex + 1
-			end
 		end
 	end
 
@@ -116,21 +138,83 @@ local function updateTopicsList(e)
 			end)
 		end
 	end
+	---mwse.log( json.encode(textPane.children,{indent = true}) ) -- something is not right, it seems GUI_ID_MenuDialog_answer_block are not added on greeting /abot
+end
+
+local function update()
+
+	--answers = {}
+
+	local function traverse(element)
+		local path = {}
+		local i = 0
+		local answerIndex = 0
+
+		local function recursiveTraverse(node)
+			if node then
+				local children = node.children
+				if children then
+					if #children > 0 then
+						for _, child in pairs(children) do
+							recursiveTraverse(child)
+						end
+					end
+				end
+				if node.id == GUI_ID_MenuDialog_answer_block then
+					local oldText = node.text
+					node:register("mouseClick", updateTopicsList)
+					answerIndex = answerIndex + 1
+					if not string.match(oldText,"^%d+") then
+						node.text = string.format("%d. %s", answerIndex, oldText)
+						answers[answerIndex] = node
+					end
+				end
+				i = i + 1
+				path[i] = node.id .. " " .. node.text
+			end
+		end
+
+		recursiveTraverse(element)
+		return path
+	end
+
+	local menuDialog = tes3ui.findMenu(GUI_ID_MenuDialog)
+	if menuDialog then
+		---mwse.log("GUI_ID_MenuDialog_hyper = %s, GUI_ID_MenuDialog_answer_block = %s", GUI_ID_MenuDialog_hyper, GUI_ID_MenuDialog_answer_block)
+		local scroll_pane = menuDialog:findChild(GUI_ID_MenuDialog_scroll_pane)
+		if scroll_pane then
+			local pane_pane = scroll_pane:findChild(GUI_ID_PartScrollPane_pane)
+			if pane_pane then
+				traverse(pane_pane)
+				--[[
+				local path = traverse(pane_pane)
+				local s = json.encode(path,{indent = true})
+				mwse.log("scroll_pane:")
+				mwse.log(s)
+				--]]
+			end
+		end
+
+	end
 end
 
 local function onDialogueMenuActivated(e)
-	-- We only care if this is the first time it was activated.
+	-- We only care if this is the node time it was activated.
 	if (not e.newlyCreated) then
 		return
 	end
 
 	-- Set the pre-update event to update the topic list.
-	answers = {}
 	e.element:register("preUpdate", function(preUpdateEventData)
 		-- We only want this event to fire once. We'll manually track changes above to be more efficient.
 		e.element:unregister("preUpdate")
 		updateTopicsList(preUpdateEventData)
 	end)
+
+	-- special as I am not able to find GUI_ID_MenuDialog_answer_block in pairs(children) on greetings /abot
+	answers = {}
+	e.element:register("update", update)
+
 end
 event.register("uiActivated", onDialogueMenuActivated, { filter = "MenuDialog" })
 event.register("keyDown", checkForAnswerHotkey)
