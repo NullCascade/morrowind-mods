2ab29d40cd7d335bd942585ead21b1e45acd8968
NullCascade <nullcascade@gmail.com>
2021-11-26 11:01:59 -0800

    UI Expansion: Add IDs to new icon/spell elements.

--- User Interface Expansion/MWSE/mods/UI Expansion/MenuServiceSpells.lua
+++ User Interface Expansion/MWSE/mods/UI Expansion/MenuServiceSpells.lua
@@ -40,11 +40,13 @@ local function onUIActivated(e)
 	local sPoints = tes3.findGMST(tes3.gmst.spoints).value
 	local MenuServiceSpells_Spell_Click = 0x616690
 	local MenuServiceSpells_Spell_Help = 0x616810
+	local GUI_ID_MenuServiceSpells_Icon = tes3ui.registerID("MenuServiceSpells_Icon")
+	local GUI_ID_MenuServiceSpells_Spell = tes3ui.registerID("MenuServiceSpells_Spell")
 
 	-- Fill it out.
 	for _, spell in ipairs(serviceSpells) do
 		local spellPrice = tes3.calculatePrice({ merchant = merchant, bartering = true, object = spell })
-		local label = MenuServiceSpells_Spells:createTextSelect({ text = string.format("%s (%d%s) - %d%s", spell.name, spell.magickaCost, sPoints, spellPrice, sGP) })
+		local label = MenuServiceSpells_Spells:createTextSelect({ id = GUI_ID_MenuServiceSpells_Spell, text = string.format("%s (%d%s) - %d%s", spell.name, spell.magickaCost, sPoints, spellPrice, sGP) })
 		label:setPropertyObject("MenuServiceSpells_Spell", spell)
 		label:register("mouseClick", MenuServiceSpells_Spell_Click)
 		label:register("help", MenuServiceSpells_Spell_Help)
@@ -54,7 +56,7 @@ local function onUIActivated(e)
 			label.widget.state = 2
 		end
 		
-		local icon = MenuServiceSpells_Icons:createImage({ path = string.format("icons\\%s", spell.effects[1].object.icon) })
+		local icon = MenuServiceSpells_Icons:createImage({ id = GUI_ID_MenuServiceSpells_Icon, path = string.format("icons\\%s", spell.effects[1].object.icon) })
 		icon.borderTop = 2
 		icon:setPropertyObject("MenuServiceSpells_Spell", spell)
 		icon:register("mouseClick", MenuServiceSpells_Spell_Click)
