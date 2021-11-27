9908bdfbba60a8d583bc06cd3880e22d7ee5cd45
NullCascade <nullcascade@gmail.com>
2021-11-26 16:30:10 -0800

    UI Expansion: Minor code changes.

--- User Interface Expansion/MWSE/mods/UI Expansion/MenuServiceSpells.lua
+++ User Interface Expansion/MWSE/mods/UI Expansion/MenuServiceSpells.lua
@@ -43,24 +43,27 @@ local function onUIActivated(e)
 	local GUI_ID_MenuServiceSpells_Icon = tes3ui.registerID("MenuServiceSpells_Icon")
 	local GUI_ID_MenuServiceSpells_Spell = tes3ui.registerID("MenuServiceSpells_Spell")
 
-	-- Fill it out.
+	-- Fill out the service list.
 	for _, spell in ipairs(serviceSpells) do
+		-- Create an icon for usability/prettiness.
+		local icon = MenuServiceSpells_Icons:createImage({ id = GUI_ID_MenuServiceSpells_Icon, path = string.format("icons\\%s", spell.effects[1].object.icon) })
+		icon.borderTop = 2
+		icon:setPropertyObject("MenuServiceSpells_Spell", spell)
+		icon:register("mouseClick", MenuServiceSpells_Spell_Click)
+		icon:register("help", MenuServiceSpells_Spell_Help)
+
+		-- Reimplement and tighten up the text a bit.
 		local spellPrice = tes3.calculatePrice({ merchant = merchant, bartering = true, object = spell })
 		local label = MenuServiceSpells_Spells:createTextSelect({ id = GUI_ID_MenuServiceSpells_Spell, text = string.format("%s (%d%s) - %d%s", spell.name, spell.magickaCost, sPoints, spellPrice, sGP) })
 		label:setPropertyObject("MenuServiceSpells_Spell", spell)
 		label:register("mouseClick", MenuServiceSpells_Spell_Click)
 		label:register("help", MenuServiceSpells_Spell_Help)
 
+		-- Gray out the text if we can't afford it.
 		if (spellPrice > playerCurrentGold) then
 			label.disabled = true
 			label.widget.state = 2
 		end
-		
-		local icon = MenuServiceSpells_Icons:createImage({ id = GUI_ID_MenuServiceSpells_Icon, path = string.format("icons\\%s", spell.effects[1].object.icon) })
-		icon.borderTop = 2
-		icon:setPropertyObject("MenuServiceSpells_Spell", spell)
-		icon:register("mouseClick", MenuServiceSpells_Spell_Click)
-		icon:register("help", MenuServiceSpells_Spell_Help)
 	end
 
 	-- Finish up.
