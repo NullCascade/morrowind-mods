93ff8c435c1abf1c3987796e25a014427f1bf941
Pete Goodfellow <petethegoat@gmail.com>
2025-01-22 01:22:58 +0000

    Update iconbar changes for modern UIEXP.

--- User Interface Expansion/MWSE/mods/UI Expansion/main.lua
+++ User Interface Expansion/MWSE/mods/UI Expansion/main.lua
@@ -59,6 +59,8 @@ local defaultConfig = {
 		tooltip = true,
 		training = true,
 	},
+	iconBarLocation = "Bottom",
+	iconBarCenterAlign = false,
 }
 local config = mwse.loadConfig("UI Expansion", defaultConfig) --- @type table<string, any>
 
